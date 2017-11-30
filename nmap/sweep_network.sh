f1cbac1abbf2dd6679a04b24b58afc2a3277d88e
Bolzon <blzn@mail.ru>
2017-11-30 21:21:52 -0200

    inserting useful links

--- nmap/sweep_network.sh
+++ nmap/sweep_network.sh
@@ -3,4 +3,4 @@
 # sweep network and brings IP of connected machines
 # cut command brings only the 5th column
 
-nmap -sn -n 192.168.1.0/24 | grep 192 | cut -d ' ' -f 5
+nmap -sn -n 192.168.1.0/24 | grep 192 # | cut -d ' ' -f 5
