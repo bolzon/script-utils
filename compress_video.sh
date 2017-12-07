06232f14d7e11f26b5d9a6f4222770fb8150356e
Bolzon <blzn@mail.ru>
2017-12-07 19:54:43 -0200

    updating compress video script

--- compress_video.sh
+++ compress_video.sh
@@ -3,3 +3,7 @@
 # compress mp4 videos
 
 ffmpeg -i input.mp4 -vf scale=iw/2:-1 output.mp4
+
+# compress mp4 video (divide 1GB by the video length in seconds - this is the 'b' parameter)
+
+ffmpeg -i input.mp4 -b 1000000 output.mp4
