08e0cf05ffc5cd0a4358df5c6a76b3a4bc924fe8
Bolzon <blzn@mail.ru>
2018-04-10 14:36:50 -0300

    adding more scripts on dotnet core folder

---
+++ dotnet-core/add-reference.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# go to the root folder of the dot net project and type:
+dotnet add reference <path_to_referenced_project>
---
+++ dotnet-core/new-projects.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# class library
+dotnet new classlib -n <project_name>
+
+# webapi
+dotnet new webapi -n <project_name>
