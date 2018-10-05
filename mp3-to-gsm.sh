3ed3916777c262291220581f73e7cba908fb981c
Alexandre Bolzon <blzn@mail.ru>
2018-10-04 21:59:17 -0300

    modifying script to convert audios to gsm (any extension)

--- mp3-to-gsm.sh
+++ mp3-to-gsm.sh
@@ -5,5 +5,19 @@
 
 #sudo apt-get install sox libsox-fmt-all
 
+if [ -z $1 ]
+then
+    echo "
+  USAGE
+    $0 <audio_extension>   # mp3, wav, avi
+"
+    exit 0;
+else
+    AUDIO_EXTENSION=$1
+fi
+
 # converts mp3 files to gsm (gsm files will be in gsm folder)
-for f in *.mp3; do F=$(echo $f | sed "s/\.mp3//"); sox $f -r 8000 -c 1 gsm/$F.gsm; done;
+for f in *.$AUDIO_EXTENSION; do
+    F=$(echo $f | sed "s/\.$AUDIO_EXTENSION//")
+    sox $f -r 8000 -c 1 gsm/$F.gsm
+done
